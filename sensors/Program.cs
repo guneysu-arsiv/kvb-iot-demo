b54c8b2f2274538adff2f5ede867151b06a711da
seref <serefguneysu@gmail.com>
2017-08-22 14:00:02 +0300

    CloudAmqp Broker integration

--- sensors/Program.cs
+++ sensors/Program.cs
@@ -1,60 +1,131 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data.Odbc;
+using System.Linq;
 using System.Reactive.Linq;
-using System.Threading;
+using System.Text;
+using Elasticsearch.Net;
+using MQTTnet;
+using MQTTnet.Core;
+using MQTTnet.Core.Client;
+using MQTTnet.Core.Packets;
+using MQTTnet.Core.Protocol;
+using Nest;
 
 namespace Sensors
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        /*
+         * http://www.hivemq.com/blog/mqtt-essentials-part-5-mqtt-topics-best-practices
+         */
+        public static void Main (string [] args)
         {
-            var manager = new SensorManager();
+            var manager = new SensorManager ( );
 
-            manager.Watch(new GasSensor(min: 0.05, max: 0.951), TimeSpan.FromMilliseconds(1000));
-            manager.Watch(new SmokeSensor(min: 0.05, max: 0.951), TimeSpan.FromMilliseconds(1000));
+            manager.Mqtt.SubscribeAsync (new TopicFilter [] {
+                //new TopicFilter ("$SYS/#", MqttQualityOfServiceLevel.AtMostOnce),
+                new TopicFilter ("sensor/#", MqttQualityOfServiceLevel.AtMostOnce),
+            }).Wait (1000);
 
-            Console.ReadKey();
+            manager.Mqtt.ApplicationMessageReceived += Client_ApplicationMessageReceived;
 
-//            Observable.Timer(TimeSpan.FromSeconds(3)).Subscribe(x =>
-//            {
-//                manager.StopAll();
-//            });
-//            Thread.Sleep(60);
+            manager.Watch (new Gas (min: 0.05, max: 0.951), TimeSpan.FromMilliseconds (1000));
+            manager.Watch (new Smoke (min: 0.05, max: 0.951), TimeSpan.FromMilliseconds (1000));
+
+            Console.ReadLine ( );
+
+            //            Observable.Timer(TimeSpan.FromSeconds(3)).Subscribe(x =>
+            //            {
+            //                manager.StopAll();
+            //            });
+            //            Thread.Sleep(60);
+        }
+
+        private static void Client_ApplicationMessageReceived (object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            var msg = float.Parse(Encoding.ASCII.GetString (e.ApplicationMessage.Payload));
+
+            Console.WriteLine ($"{e.ApplicationMessage.Topic}: {msg}");
+        }
+    }
+
+    public class ExampleMqttBrokers
+    {
+        public IMqttClient CloudMqtt ( )
+        {
+            return new MqttClientFactory ( ).CreateMqttClient (new MqttClientOptions
+            {
+                ClientId = Guid.NewGuid ( ).ToString ( ),
+                Port = 12662,
+                UserName = "vihvzpqe",
+                Password = "MJN_Kj7uz1lB",
+                Server = "m20.cloudmqtt.com",
+            });
+        }
+
+        public IMqttClient Eclipse ( )
+        {
+            return new MqttClientFactory ( ).CreateMqttClient (new MqttClientOptions
+            {
+                ClientId = Guid.NewGuid ( ).ToString ( ),
+                Port = 1883,
+                Server = "iot.eclipse.org",
+            });
         }
     }
 
     public class SensorManager
     {
-        List<IDisposable> _subscriptions = new List<IDisposable>();
-        List<Sensor> _sensors = new List<Sensor>();
+        public IMqttClient Mqtt { get; protected set; }
+        public ElasticSearchClient<object> ElasticSearch { get; protected set; }
 
-        public void Watch(Sensor sensor, TimeSpan interval)
+        List<IDisposable> _subscriptions = new List<IDisposable> ( );
+        List<Sensor> _sensors = new List<Sensor> ( );
+
+        public void Watch (Sensor sensor, TimeSpan interval)
         {
-            _sensors.Add(sensor);
+            _sensors.Add (sensor);
 
-            _subscriptions.Add(Observable.Interval(interval)
-                .Subscribe(x => { Console.WriteLine(sensor.Read()); }));
+            this._subscriptions.Add (Observable.Interval (interval)
+                .Subscribe (x =>
+                {
+                    var value = sensor.Read ( );
+                    Mqtt.PublishAsync (
+                        new MqttApplicationMessage (
+                            topic: $"sensor/{sensor.GetType ( ).Name}".ToLower(),
+                            payload: Encoding.ASCII.GetBytes (value.ToString ( )),
+                            qualityOfServiceLevel: MqttQualityOfServiceLevel.ExactlyOnce,
+                            retain: true));
+                }));
         }
-
-        public void StopAll()
+        public SensorManager ( )
         {
-            _subscriptions.ForEach(x => x.Dispose());
-            _subscriptions.Clear();
+            Mqtt = new ExampleMqttBrokers ( ).CloudMqtt ( );
+            this.ElasticSearch = new ElasticSearchClient<object> ("http://elastic:changeme@localhost:9200", "iot");
+            Mqtt.ConnectAsync ( ).Wait (1000);
+
+        }
+        public void StopAll ( )
+        {
+            _subscriptions.ForEach (x => x.Dispose ( ));
+            _subscriptions.Clear ( );
         }
     }
 
     public abstract class Sensor
     {
-        protected Sensor(double min, double max)
+        public Sensor ( )
+        {
+            Guid = Guid.NewGuid ( );
+        }
+
+        protected Sensor (double min, double max)
         {
             _min = min;
             _max = max;
-            _random = new Random();
+            _random = new Random ( );
 
-            Observable.Interval(TimeSpan.FromMilliseconds(100)).Subscribe(x => { _current = NextValue(); });
+            Observable.Interval (TimeSpan.FromMilliseconds (100)).Subscribe (x => { _current = NextValue ( ); });
         }
 
         protected double _max { get; }
@@ -62,30 +133,108 @@ namespace Sensors
         protected double _min { get; }
         protected Random _random { get; set; }
 
+        protected Guid Guid { get; set; }
 
-        public double Read()
+        public string Id => Guid.ToString ( );
+
+        public double Read ( )
         {
             return _current;
         }
 
-        public double NextValue()
+        public double NextValue ( )
         {
-            var value = _min + (_max - _min) * _random.NextDouble();
+            var value = _min + ( _max - _min ) * _random.NextDouble ( );
             return value;
         }
     }
 
-    public class GasSensor : Sensor
+    public class Gas : Sensor
     {
-        public GasSensor(double min, double max) : base(min, max)
+        public Gas (double min, double max) : base (min, max)
         {
         }
     }
 
-    public class SmokeSensor : Sensor
+    public class Smoke : Sensor
     {
-        public SmokeSensor(double min, double max) : base(min, max)
+        public Smoke (double min, double max) : base (min, max)
         {
+        }
+    }
+
+
+    public class ElasticSearchClient<T> where T : class, new()
+    {
+        public ElasticSearchClient (string connString, string defaultIndex)
+        {
+            var node = new Uri (connString);
+            var connectionPool = new SniffingConnectionPool (new [] { node });
+
+            var config = new ConnectionConfiguration (connectionPool);
+
+            var pool = new SingleNodeConnectionPool (node);
+
+            var settings = new ConnectionSettings (pool)
+                .DefaultIndex (defaultIndex);
+
+            Client = new ElasticClient (settings);
+        }
+
+        public ElasticSearchClient (IConnectionSettingsValues settings)
+        {
+            Client = new ElasticClient (settings);
+        }
+
+        public static ConnectionSettings CreateSettings (Uri uri, string indexName)
+        {
+            return new ConnectionSettings (uri: uri)
+                .DefaultIndex (defaultIndex: indexName)
+                ;
+        }
+
+        public ElasticClient Client { get; set; }
+
+        public void DeleteIndexIfExists (string index)
+        {
+            if ( Client.IndexExists (index).Exists )
+                Client.DeleteIndex (index);
+        }
+
+        public IBulkResponse Index (IEnumerable<T> data, string pipeline)
+        {
+            IList<IBulkOperation> ops = data.Select (e => new BulkIndexOperation<T> (e)
+            {
+                Pipeline = pipeline
+            }).Cast<IBulkOperation> ( ).ToList ( );
+
+            var br = new BulkRequest ( )
+            {
+                Pipeline = pipeline,
+                Operations = ops
+            };
+
+            return Client.Bulk (br);
+        }
+
+        public IBulkResponse Index (IEnumerable<T> data)
+        {
+            return Client.IndexMany (data);
+        }
+
+        public IIndexResponse Index (T data)
+        {
+            return Client.Index (data);
+        }
+
+        public void SwapAlias (string index)
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public IBulkResponse Index (IQueryable<T> data)
+        {
+            return Client.IndexMany (objects: data);
         }
     }
 }
